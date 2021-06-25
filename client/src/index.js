777a29a71edbc3c6795b7e925e20b6a26fbaadce
mainlyetcetera <ericcampbell294@gmail.com>
2021-06-24 19:19:47 -0600

    Render basic header

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router } from 'react-router-dom'
 import './index.css'
-import App from './App'
+import App from './components/App/App'
 
 ReactDOM.render(
   <Router>
