f2112571d188c9b6523284851e2bcefbf0af5836
mainlyetcetera <ericcampbell294@gmail.com>
2021-06-24 19:45:48 -0600

    Build Link, return home page

--- client/src/components/Champions/Champions.js
+++ client/src/components/Champions/Champions.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Champions() {
+
   return (
-    <h3>We are the champions...</h3>
+    <>
+      <h3>We are the champions...</h3>
+      <Link to='/'>Go back</Link>
+    </>
   )
 }
 
