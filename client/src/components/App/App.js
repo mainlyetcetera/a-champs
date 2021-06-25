a33ad5ca810450e556a387509efaee9d2db8ff3d
mainlyetcetera <ericcampbell294@gmail.com>
2021-06-24 19:36:26 -0600

    Render Champions component

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Route } from 'react-router-dom'
+import { Route, Link } from 'react-router-dom'
 import Header from '../Header/Header'
+import Champions from '../Champions/Champions'
 import './App.css'
 
 function App() {
@@ -8,11 +9,19 @@ function App() {
     <div className="App">
       <Header />
       <Route
+        exact path='/champions'
+        render={() => (
+          <>
+            <Champions />
+          </>
+        )}
+      />
+      <Route
         exact path='/'
         render={() => (
           <>
-            App started...
-            <h1>random junk</h1>
+            <h4>Don't worry, it's not a Pokemon app...</h4>
+            <Link to='/champions'>See some champs...</Link>
           </>
         )}
       />
---
+++ client/src/components/Champions/Champions.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Champions() {
+  return (
+    <h3>We are the champions...</h3>
+  )
+}
+
+export default Champions
