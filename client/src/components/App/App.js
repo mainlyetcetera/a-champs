c60df1f1b4f75b351efc3c34e832a1aa6ad812b9
mainlyetcetera <ericcampbell294@gmail.com>
2021-06-24 19:22:22 -0600

    Implement first route, basic render

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
 import Header from '../Header/Header'
 import './App.css'
 
@@ -6,7 +7,15 @@ function App() {
   return (
     <div className="App">
       <Header />
-      App started...
+      <Route
+        exact path='/'
+        render={() => (
+          <>
+            App started...
+            <h1>random junk</h1>
+          </>
+        )}
+      />
     </div>
   )
 }
