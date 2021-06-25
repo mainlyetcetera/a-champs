f90173c329a4bd08dfd96915545bc3a12c73dc38
mainlyetcetera <ericcampbell294@gmail.com>
2021-06-24 19:19:33 -0600

    Modularize components

---
+++ client/src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../Header/Header'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      App started...
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <h2>Here's a header</h2>
+  )
+}
+
+export default Header
